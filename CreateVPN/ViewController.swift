1d3316098725a19cc19b66298e66d3c403b98db4
Oriol Marí Marqués <Oriol@Oriols-MacBook-Pro.local>
2016-11-17 17:19:10 +0100

    Initial Commit

---
+++ CreateVPN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreateVPN
+//
+//  Created by Oriol Marí Marqués on 17/11/2016.
+//  Copyright © 2016 Oriol Marí Marqués. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
